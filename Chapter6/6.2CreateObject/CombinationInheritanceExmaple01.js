64c3bbc80d37236381d1c0913d65dfdcbd2bbcd2
liuchao <ry2u@outlook.com>
2018-04-22 12:06:43 +0800

    combination inheritance

---
+++ Chapter6/6.2CreateObject/CombinationInheritanceExmaple01.js
@@ -0,0 +1,30 @@
+function SuperType(name){
+    this.name =  name;
+    this.colors = ["red","blue","green"];
+}
+
+SuperType.prototype.sayName = function(){
+    console.log(this.name);
+};
+
+function SubType(name,age){
+    SuperType.call(this,name);
+    this.age = age;
+}
+
+SubType.prototype= new SuperType();
+SubType.prototype.constructor = SubType();
+SubType.prototype.sayAge= function(){
+    console.log(this.age);
+}
+
+var instance1 =new SubType("Nick",29);
+instance1.colors.push("black");
+console.log(instance1.colors);
+instance1.sayName();
+instance1.sayAge();
+
+var instance2 =new SubType("Greg",27);
+console.log(instance2.colors);
+instance2.sayName();
+instance2.sayAge();
